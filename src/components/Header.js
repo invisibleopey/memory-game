2b97dceaa0ab91db8e69f3a18d237527af0a97bd
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-02-20 00:41:06 +0100

    Implement header component with headings

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header" data-testid="header">
+      <h1>Comrade Memory Game</h1>
+    </header>
+  );
+}
+
+export default Header;
