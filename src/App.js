b3a49dac67397b2e8852c5e49fe6ef1acc36193a
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-02-20 23:57:10 +0100

    Import and render the Cards component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './styles/App.css';
 import Header from '../src/components/Header';
+import Cards from './components/Cards';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <Cards />
+      </main>
     </div>
   );
 }
