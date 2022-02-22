9ebdedea3d846701af9645c2ad50421742162904
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-02-22 17:38:40 +0100

    pass down props to scoreboard component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Scoreboard from './Scoreboard';
 
-function Header() {
+function Header(props) {
   return (
     <header className="header" data-testid="header">
       <h1>Comrade Memory Game</h1>
-      <Scoreboard />
+      <Scoreboard score={props.score} bestScore={props.bestScore} />
       <p>Get points by clicking on an image but don't click on any more than once!</p>
     </header>
   );
