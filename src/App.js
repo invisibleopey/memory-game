862fee132600e854b139fbc207fd40c0e0d34b49
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-02-22 17:09:54 +0100

    Create function that shuffles divs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import Header from '../src/components/Header';
 import Cards from './components/Cards';
 
 function App() {
+  const shuffleDivs = () => {
+    let parent = document.querySelector('.cards');
+    let divs = parent.children;
+    let frag = document.createDocumentFragment();
+    while (divs.length) {
+      frag.appendChild(divs[Math.floor(Math.random() * divs.length)]);
+    }
+    parent.appendChild(frag);
+  };
+  const handleSelection = () => {};
   return (
     <div className="App">
       <Header />
       <main>
-        <Cards />
+        <Cards handleSelection={handleSelection} />
       </main>
     </div>
   );
